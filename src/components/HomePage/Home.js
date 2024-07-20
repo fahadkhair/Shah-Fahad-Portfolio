78ede8b74835a5fc7c5214e0abd35634a05c29ea
Fahad Khair <fahadkhair21@gmail.com>
2024-07-20 00:41:46 -0700

    Pic size corrected

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <h1 className='anim'>Shah <span>Fahad </span></h1>
         <p className='anim'>Innovative thinker and passionate creator, dedicated to transforming ideas into impactful digital experiences.</p>
         <p className='anim'> 👋 Hi, I'm Shah Fahad, a passionate individual diving into the realms of technology and creativity. With a keen eye for innovation and a heart set on learning, I'm on a journey to explore the endless possibilities that the digital world offers.</p>
-        <a href="/" className='home-btn anim'>Contact Now!</a>
+        <a href="/Contact" className='home-btn anim'>Contact Now!</a>
       </div>
       
       <img src={dpImage} className='home-dp anim' />
